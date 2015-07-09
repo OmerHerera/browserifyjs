6ce53cda9f73367a302e84b461f8bc63843ac4f4
Omer Herera <omerh@liveperson.com>
2015-07-09 16:30:37 +0300

    refactor build + reactJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,37 @@
 module.exports = function(grunt) {
-    var path = require('path');
-
     require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt, {
-        // path to task.js files, defaults to grunt dir
-        configPath: path.join(process.cwd(), 'build'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-        // auto grunt.initConfig
-        init: true,
-
-        // data passed into config.  Can use with <%= test %>
-        data: {
-            test: false
+        watch: {
+            react: {
+                files: './frameworks/reactJS/*.jsx',
+                tasks: ['browserify']
+            }
         },
-
-        // can optionally pass options to load-grunt-tasks.
-        // If you set to false, it will disable auto loading tasks.
-        loadGruntTasks: {
-            pattern: ['grunt-*'],
-            config: require('./package.json'),
-            scope: 'devDependencies'
-        },
-
-        //can post process config object before it gets passed to grunt
-        postProcess: function(config) {},
-
-        //allows to manipulate the config object before it gets merged with the data object
-        preMerge: function(config, data) {}
+        browserify: {
+            vanillaJS: {
+                "src": "./frameworks/vanillaJS/main.js",
+                "dest": "./frameworks/vanillaJS/main-min.js"
+            },
+            "backbone": {
+                "src": "./frameworks/backbone/main.js",
+                "dest": "./frameworks/backbone/main-min.js"
+            },
+            reactJS: {
+                options: {
+                    transform: [require('grunt-react').browserify]
+                },
+                src: ['./frameworks/reactJS/*.jsx'],
+                dest: './frameworks/reactJS/main-min.js'
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', [
+        'browserify'
+    ]);
 };
--- build/aliases.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt, options) {
-
-    var tasks = ['browserify'];
-
-    return {
-        'tasks': ['availabletasks'],
-        'default': tasks
-    };
-};
